eddd60dae113b1793725f6127b810632d8eec37c
loganbaby <shifo3456@gmail.com>
2023-02-13 19:55:51 +0300

    fix balance promise

--- index.js
+++ index.js
@@ -11,13 +11,12 @@ const serverPort = 8000;
 
 app.set("view engine", "ejs");
 
-var res;
 app.use("/", function(request, response) {
-	myAPI.getBalance().then(data => res = data).then( _ => {
-		console.log(res.balance_bitcoin);
+	myAPI.getBalance().then(data => {
+		console.log(data.balance_bitcoin);
 
 		response.render("index", {
-			btcBalance: res.balance_bitcoin,
+			btcBalance: data.balance_bitcoin,
 			title: "Crypto faucet"
 		})}
 	);
